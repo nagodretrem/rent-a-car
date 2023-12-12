92a46b9f553de38a9f91be8b9834b86f9e5f0aa2
Mert <nagodretrem@gmail.com>
2023-12-12 19:59:16 +0300

    added repositories

---
+++ src/main/java/com/tobeto/rentacar/repositories/InsuranceCompanyRepository.java
@@ -0,0 +1,7 @@
+package com.tobeto.rentacar.repositories;
+
+import com.tobeto.rentacar.entities.InsuranceCompany;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InsuranceCompanyRepository extends JpaRepository<InsuranceCompany,Integer> {
+}
